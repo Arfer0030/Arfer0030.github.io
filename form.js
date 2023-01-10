e016857dcfc11095b19ef7559cf43cd8b6c16304
Arfer0030 <aufarafli1234@gmail.com>
2023-01-10 22:47:45 +0700

    first commit

---
+++ form.js
@@ -0,0 +1,16 @@
+var nama = prompt('masukan nama')
+alert('hai ' + nama + ' cantik 👋🏽')
+alert('Selamat Ulang Tahun yaaa ' + nama)
+alert('ciyee dah 18 tahun aja')
+alert('semoga ' + nama + ' panjang umur')
+alert('dan  sehat selalu yaaa') 
+var mau = confirm('kamu mau kukasih hadiah nggaa?')
+    if( mau === true ) {
+        alert('okey sayang')
+        alert('abis ni dipencet yaa itu')
+    } else {
+        alert('dih tai🖕')
+        alert('tapi tetep kukasi')
+        alert('karena gweh baik😌😒')
+        alert('pencet tuh sana')
+    }
